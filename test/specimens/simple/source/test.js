7d7b2d7cd146e2aa2c912110522bff585eb86cc3
Pat Cavit <pcavit@gmail.com>
2013-09-12 01:03:59 -0700

    Test specimens

---
+++ test/specimens/simple/source/test.js
@@ -0,0 +1,8 @@
+// http://eloquentjavascript.net/chapter2.html#exercise2
+var result = 1;
+var counter = 0;
+while (counter < 10) {
+  result = result * 2;
+  counter = counter + 1;
+}
+console.log(result);
